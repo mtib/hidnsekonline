d70d3cdf1f25ffa74fda673d3e0c258878594417
Markus Becker <mtb@tibyte.net>
2015-12-06 13:19:31 +0100

    setup, connections

--- client.py
+++ client.py
@@ -4,25 +4,119 @@ import threading
 
 class Game(object):
     """docstring for Game"""
-    def __init__(self):
-        self.serverip = input("Server adress: ")
-        self.serverport = input("Server port: [default:2020] ")
-        if len(self.serverport):
-            try:
-                self.serverport = int(self.serverport)
-            except:
-                self.serverport = 2020
-        else:
-            self.serverport = 2020
+    def __init__(self, debug=False):
+        self.connected = False
+        self.debug = debug
+        explainstr = """
+        Welcome to hidnsekonline
+        there are two parties in this game, the escapees and the murderer.
+
+        ESCAPEE: you have to find your companion, and decide
+        for a spot to hide together (there's a chat).
+        But after a while (1 minute) there will be a murderer on the hunt
+        for you.
+
+        MURDERER: is somehow psychic because he can read your chat and is
+        skilled in reading footprints.
+
+        to move type /left /up /right or /down in chat
+        to hide type /hide
+        to kill type /kill (one chance or the escapees win)
+        to quit type /quit
+        """
+        print(explainstr)
+
+
+    def configure(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = True
+        if self.debug:
+            self.serverip = "localhost"
+            self.serverport = 2020
+            self.username = "player_{}".format(int(time.time())).split()[0]
+            print("Username:",self.username)
+        else:
+            self.serverip =   input("Server adress: [default:localhost] ")
+            if len(self.serverip)==0:
+                self.serverip = "localhost"
+            self.serverport = input("Server port:   [default:2020]      ")
+            if len(self.serverport):
+                try:
+                    self.serverport = int(self.serverport)
+                except:
+                    self.serverport = 2020
+            else:
+                self.serverport = 2020
+            self.username = input("Username: ").split()[0]
+            #self.send("/username {}".format(self.username))
         self.socket.connect((self.serverip,self.serverport))
         self.send("/connect")
-        self.username = input("Username: ")
+        time.sleep(.3)
         self.send("/username {}".format(self.username))
+
+    def connect(self):
+        self.type = None
+        self.hidden = False
+        import random
         while True:
             servermsg = str(self.socket.recv(1024), "utf-8")
             if servermsg.startswith("/waiting "):
                 print(servermsg[1:])
+                self.send("/ok wait")
+                continue
+            if servermsg.startswith("/you "):
+                self.type = servermsg[5:]
+                if self.type == "ESCAPEE":
+                    print("game started!\nyou are an ESCAPEE\ntry to find a place to hide with your companion")
+                elif self.type == "MURDERER":
+                    print("game started!\nyou are the MURDERER\nthe ESCAPEES have 1 minute to hide")
+                    time.sleep(55)
+                    self.posx = random.randrange(3)
+                    self.posy = random.randrange(3)
+                    self.printmap()
+                else:
+                    print("protocol mishab")
+                    raise
+                continue
+            if servermsg.startswith("/youpos ") and self.type and not self.hidden:
+                cmd, x, y = servermsg.split()
+                self.posx = int(x)
+                self.posy = int(y)
+                self.printmap()
+                command = str(input(">> "))
+
+    def drawmap(self, x,y, pre="  ", you="X", unknown="?", lines=["","",""]):
+        m = "\n"
+        for ty in range(3):
+            for tx in range(3):
+                if tx == 0:
+                    m += str(pre)
+                    m +="["
+                else:
+                    m +="-["
+                if tx == x and ty == y:
+                    m += str(you)
+                else:
+                    m += str(unknown)
+                m+= "]"
+            if ty == 1:
+                m+="\t\t"
+                m+=str(lines[1])
+            m+="\n"
+            if ty < 2:
+                m+=str(pre)
+                m+=" |   |   |\t\t"
+                if ty == 0:
+                    m+=str(lines[0])
+                if ty == 1:
+                    m+=str(lines[2])
+                m+="\n"
+        return m
+
+    def printmap(self):
+        if self.type == "ESCAPEE":
+            m = self.drawmap(self.posx, self.posy, lines=["You are at the X","use /left, /right, /up or /down","to move. Find your companion"])
+            print(m)
 
     def send(self, msg):
         self.socket.send(msg.encode("utf-8"))
@@ -33,8 +127,18 @@ class Game(object):
 
 
 def main(args):
-    client = Game()
+    try:
+        debug = False
+        if len(args)>1:
+            debug = args[1]=="debug"
+        client = Game(debug)
+        client.configure()
+        client.connect()
+    except KeyboardInterrupt:
+        if client.connected:
+            client.disconnect()
+        sys.exit()
 
 if __name__ == '__main__':
-    from sys import argv
-    main(argv)
+    import sys
+    main(sys.argv)
--- server.py
+++ server.py
@@ -26,13 +26,18 @@ class Gameserver:
         self.server = Server(port, maxplayer)
         self.live = 0
         self.stat = []
+        self.livecon = {}
 
     def start(self):
-        self.server.start()
+        try:
+            self.server.start()
+        except OSError as e:
+            print("Port or adress already in use!\nTerminating")
+            sys.exit()
         self.running = True
         while self.running:
             try:
-                threading.Thread(target=self.handle,args=self.server.accept()).start()
+                threading.Thread(target=self.handle,args=self.server.accept(),daemon=True).start()
             except Exception as e:
                 self.stop()
 
@@ -45,42 +50,127 @@ class Gameserver:
         self.live += 1
         def smsg(msg):
             conn.send(msg.encode("utf-8"))
-        connect = str(conn.recv(1024), "utf-8")
+        connect = str(conn.recv(64), "utf-8")
         if connect.startswith("/connect"):
-            print("new client connected [{}]".format(self.live))
+            toconsole("new client connected [{}]".format(self.live))
         else:
-            print("client using wrong protocol ({}) to connect: \"{}\"".format(addr[0],connect))
+            toconsole("client using wrong protocol ({}) to connect: \"{}\"".format(addr[0],connect))
+            self.live -= 1
             return False
-        username = str(conn.recv(1024), "utf-8")
+        username = str(conn.recv(2048), "utf-8")
         if username.startswith("/username "):
-            username = username[10:]
+            username = username[10:].split()[0]
         else:
-            print("client using wrong protocol ({}) for username".format(addr[0]))
+            toconsole("client using wrong protocol ({}) for username".format(addr[0]))
+            self.live -= 1
             return False
+        toconsole("{} joined the game".format(username))
         self.stat.append(username)
+        self.livecon[username]=(conn, self.live)
+        if self.live == 3:
+            self.giveRoles()
         while self.running:
             try:
                 if self.live < 3:
-                    smsg("/waiting for other players [{}]".format(self.stat))
-                    time.sleep(5.0)
+                    smsg("/waiting for other players [{}/3]".format(self.live))
+                    time.sleep(.1)
+                    if str(conn.recv(1024), "utf-8") == "/ok wait":
+                        time.sleep(5.0)
+                    else:
+                        raise Exception
                 else:
                     pass
             except Exception as e:
                 break
         conn.close()
+        del self.livecon[username]
+        self.stat.remove(username)
         self.live -= 1
+        toconsole("{} left the game".format(username))
+
+    def smsl(self, conn, msg):
+        conn.send(msg.encode("utf-8"))
+
+    def giveRoles(self):
+        self.field = [[{},{},{}],[{},{},{}],[{},{},{}]]
+        self.escapee = []
+        import random
+        for row in self.field:
+            for cell in row:
+                cell["history"]=""
+                cell["player"]=[]
+        for key in self.livecon:
+            if self.livecon[key][1] == 1:
+                self.smsl(self.livecon[key][0],"/you MURDERER")
+                toconsole("{} is MURDERER".format(key))
+                self.murderer = [key]
+            else:
+                scapcon = self.livecon[key][0]
+                self.smsl(scapcon,"/you ESCAPEE")
+                x=random.randrange(0,3)
+                y=random.randrange(0,3)
+                self.field[y][x]["player"].append(key)
+                self.field[y][x]["history"]+="B"
+                toconsole("{} is ESCAPEE in [{};{}]".format(key,x,y))
+                self.escapee.append(key)
+                self.smsl(scapcon, "/youpos {} {}".format(x,y))
+
+    def sendall(self, msg):
+        for key in self.livecon:
+            self.smsl(self.livecon[key][0],msg)
+
+    def sendto(self, client, msg):
+        for key in self.livecon:
+            if key == client:
+                self.smsl(self.livecon[key][0],msg)
+
+def toconsole(msg):
+    print("{}\nCommand: ".format(msg), end="")
 
 if __name__ == '__main__':
-    s = Gameserver(2000)
-    threading.Thread(target=s.start).start()
+    s = Gameserver(2020)
+    server = threading.Thread(target=s.start, daemon=True)
+    server.start()
+    helpstr = """
+    help
+        show this list
+    stat
+        returns list of all players
+    live
+        returns number of live connections
+    lcon
+        returns dictionary of player pos
+    kill
+        kills the server
+    sendall <msg>
+        sends the msg to all players
+    send <player> <msg>
+        sends the msg to player
+    """
     while True:
-        cmd = input("Command: ")
-        if cmd == "stat":
-            print(s.stat)
-        elif cmd == "live":
-            print(s.live)
-        elif cmd == "kill":
-            s.stop()
-            del s
-            break
+        try:
+            cmd = input("Command: ")
+            cml = cmd.split()
+            if cml[0] == "stat":
+                print(s.stat)
+            elif cml[0] == "help":
+                print(helpstr)
+            elif cml[0] == "live":
+                print(s.live)
+            elif cml[0] == "kill":
+                server.join(.5)
+                s.stop()
+                del s
+                break
+            elif cml[0] == "lcon":
+                print(s.livecon)
+            elif cml[0] == "sendall":
+                s.sendall(cmd[8:])
+            elif cml[0] == "send":
+                s.sendto(cml[1], cml[2])
+        except IndexError:
+            print("wrong command")
+        except KeyboardInterrupt:
+            pass
+
     sys.exit()
